c51cec7e618284f86e387b276270e7f6b6af3e57
Subhadeep-Adhikary <subhadeepadhikary049@gmail.com>
2026-06-16 13:26:16 +0530

    creating an random reactv object

--- myreact1/src/main.jsx
+++ myreact1/src/main.jsx
@@ -1,8 +1,35 @@
-import { StrictMode } from 'react'
+import React,{ StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <App />
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom APP!</h1>
+        </div>
+    )
+}
+
+// const Reactelement={
+//     type:'a',
+//     props:{
+//         href:'https://google.com',
+//         target:'_blank'
+//     },
+//     children:'Click me to visit google now'
+// }
+
+const reactelement=React.createElement(
+    'a',
+    {href:'https://google.com',
+        target:'_blank'},
+    'Click here to visit google'
+)
+
+const anotherElement=(
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+createRoot(document.getElementById('root')).render(
+   reactelement
 )

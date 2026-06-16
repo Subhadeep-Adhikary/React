52b42744d39f13c4350a2e4aaa2667b1d734d0ad
Subhadeep-Adhikary <subhadeepadhikary049@gmail.com>
2026-06-16 22:51:26 +0530

    bg changer made with react

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -6,26 +6,25 @@ import './App.css'
 import A from "./components/nav"
 
 function App() {
-  const[color,setcolor]=useState("olive")
+  const[bcolor,setbcolor]=useState("olive")
 
   return (
    <>
-  {/* Navbar */}
   <div className="fixed top-0 left-0 w-full h-16 bg-gray-800 flex items-center px-6 shadow-md">
    <nav className="flex justify-center space-x-4">
-      <A onClick={setcolor(Yellow)} color="Yellow"/>
-      <A onClick={setcolor(Green)} color="Green"/>
-      <A onClick={setcolor(Red)} color="Red"/>
-      <A onClick={setcolor(Blue)} color="Blue"/>
-      <A onClick={setcolor(Pink)} color="Pink"/>
-      <A onClick={setcolor(Grey)} color="Grey"/>
-      <A onClick={setcolor(Orange)} color="Orange"/>
-      <A onClick={setcolor(Indigo)} color="Indigo"/>
+      <A onClick={() => setbcolor("Yellow")} color="Yellow" />
+      <A onClick={() => setbcolor("Green")} color="Green" />
+      <A onClick={() => setbcolor("Red")} color="Red" />
+      <A onClick={() => setbcolor("Blue")} color="Blue" />
+      <A onClick={() => setbcolor("Pink")} color="Pink" />
+      <A onClick={() => setbcolor("Grey")} color="Grey" />
+      <A onClick={() => setbcolor("Orange")} color="Orange" />
+      <A onClick={() => setbcolor("Indigo")} color="Indigo" />
 </nav>
   </div>
 
 
-  <div className="mt-16 border border-black rounded-xl w-screen h-[calc(100vh-4rem)] flex items-center justify-center" style={{backgroundColor:color}}>
+  <div className="mt-16 border border-black rounded-xl w-screen h-[calc(100vh-4rem)] flex items-center justify-center" style={{backgroundColor:bcolor}}>
   
   </div>
 </>
--- bgchanger/src/components/nav.jsx
+++ bgchanger/src/components/nav.jsx
@@ -1,13 +1,16 @@
-function A({ color }) {
+function A({ color, onClick }) {
 
    return (
     <>
-     <a className=" font-bold rounded-lg px-3 py-2 text-gray-700 hover:bg-blue-500 hover:text-gray-900 border border-black m-20" style={{backgroundColor:color}}>
-    {color}
-  </a>
+     <a
+       onClick={onClick}
+       className="font-bold rounded-lg px-3 py-2 text-gray-700 hover:bg-blue-500 hover:text-gray-900 border border-black m-20 cursor-pointer"
+       style={{ backgroundColor: color }}
+     >
+       {color}
+     </a>
     </>
-    
-)
+ )
 }
 
 export default A
